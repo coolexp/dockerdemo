3be5424cbc90a9034e77b2f62bc7fbecfc52376e
sloppy <caozhuhua@gmail.com>
2020-06-22 17:45:59 +0800

    Init

---
+++ src/main/java/com/leaplearner/dockerdemo/HelloController.java
@@ -0,0 +1,19 @@
+package com.leaplearner.dockerdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping(value = "/foo", produces = "application/json;charset=UTF-8")
+    public String foo(HttpServletRequest request) {
+        return "foo";
+    }
+    @RequestMapping(value = "/bar", produces = "application/json;charset=UTF-8")
+    public String bar(HttpServletRequest request) {
+        return "bar";
+    }
+}
